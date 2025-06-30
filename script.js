6901c6841d4f566309189505258acf754cb72cb9
sulyman002 <oyedelesulaiman@gmail.com>
2025-06-30 09:41:47 +0100

    done with the code

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const 
+const popUp = document.querySelector('.popUp');
+const show = document.querySelector('.show')
+const michelle_profile = document.querySelector('.michelle_profile');
+const profile_container = document.querySelector('.profile_container');
+    let isDisplay = false;
+popUp.addEventListener('click', () => {
+    show.classList.toggle('hidden');
+    michelle_profile.classList.toggle('hidden');
+    
+
+
+})
